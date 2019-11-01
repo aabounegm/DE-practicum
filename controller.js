dd2c76c2fa6824a7c900ec996c0ec872181b235b
Abdelrahman Aly Abounegm <abounegm.abdelrahman@gmail.com>
2019-11-01 22:52:12 +0300

    Add comments (docs and types) and improve defaults

--- controller.js
+++ controller.js
@@ -1,23 +1,38 @@
 import { NumericalMethods, DifferentialFunction } from './models.js';
 
+// @ts-ignore
+var Chart = window.Chart;
+
 export default class ChartController {
-	constructor(funcs, { x0, y0, X, h } = {}) {
+	/**
+	 * 
+	 * @param {DifferentialFunction} funcs The function pair (exact and derivative) to compute )
+	 * @param {Object<string, number>} values The parameters based on which to calculate the approximations
+	 * @param {Object<string, HTMLInputElement>} elements The input elements to listen for changes on
+	 */
+	constructor(funcs, { x0 = 0, y0 = 0, X = 1, h = 0.1 } = {}, { x0El, y0El, XEl, hEl } = {}) {
 		this.funcs = funcs;
 
 		Chart.defaults.global.elements.line.fill = false;
+
+		// @ts-ignore
 		this.ctx = document.getElementById('graph').getContext('2d');
 		this.chart = null;
 
+		/** @type {Object<String, {el: HTMLInputElement, val: number}>} */
 		this.vars = {
-			x0: { el: document.getElementById('x0'), val: x0 },
-			y0: { el: document.getElementById('y0'), val: y0 },
-			X: { el: document.getElementById('X'), val: X },
-			h: { el: document.getElementById('h'), val: h },
+			x0: { el: x0El || document.getElementById('x0'), val: x0 },
+			y0: { el: y0El || document.getElementById('y0'), val: y0 },
+			X: { el: XEl || document.getElementById('X'), val: X },
+			h: { el: hEl || document.getElementById('h'), val: h },
 		};
 
 		this._registerListeners();
 	}
 
+	/**
+	 * Refreshes the chart using the latest input parameters
+	 */
 	buildChart() {
 		if (this.chart)
 			this.chart.destroy();
